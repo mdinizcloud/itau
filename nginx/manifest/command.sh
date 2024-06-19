c7966effd38a20d7d15b1b5d3162d87a62712fdd
Marcelo Diniz <marcelo.diniz.ti@gmail.com>
2024-06-19 12:13:13 -0300

    marcelo

--- nginx/manifest/command.sh
+++ nginx/manifest/command.sh
@@ -4,9 +4,7 @@ k apply -f rbac.yaml
 k apply -f default-server-secret.yaml
 k apply -f nginx-config.yaml
 k apply -f ingress-class.yaml
-
 k apply -f crds.yaml
-
 k apply -f nginx-ingress_daemonset.yaml
 k apply -f loadbalancer-aws-elb.yaml
 
