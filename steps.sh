04d9a1022759e2e8b6f0ff91b964ab21c5164fb6
Marcelo Diniz <marcelo.diniz.ti@gmail.com>
2024-06-19 07:18:24 -0300

    marcelo

--- steps.sh
+++ steps.sh
@@ -10,3 +10,9 @@ k -n nginx-ingress port-forward svc/nginx-ingress 80   #yaml
 change dns
 
 k apply -f ingress-resource.yaml
+
+curl http://localhost
+
+curl --connect-to www.itau.com.br:80:127.0.0.1 http://www.itau.com.br
+
+curl --connect-to www.itau.com.br::127.0.0.1 http://www.itau.com.br
