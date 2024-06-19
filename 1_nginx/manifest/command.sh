2683d46b636f3ad90c4002baffeb6e7142cbfac3
Marcelo Diniz <marcelo.diniz.ti@gmail.com>
2024-06-19 11:06:17 -0300

    marcelo

--- 1_nginx/manifest/command.sh
+++ 1_nginx/manifest/command.sh
@@ -7,10 +7,7 @@ k apply -f ingress-class.yaml
 
 k apply -f crds.yaml
 
-k apply -f crds/
-k apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/v3.5.2/deploy/crds.yaml
-
-k apply -f nginx-ingress_daemonsets.yaml
+k apply -f nginx-ingress_daemonset.yaml
 k apply -f loadbalancer-aws-elb.yaml
 
 
