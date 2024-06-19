4e7eea4c402ee1b81761da09af5ea0c6f3a9cf5d
Marcelo Diniz <marcelo.diniz.ti@gmail.com>
2024-06-19 07:55:50 -0300

    marcelo

--- steps.sh
+++ steps.sh
@@ -5,6 +5,8 @@ aws eks --region $(t output -raw region) update-kubeconfig --name $(t output -ra
 k apply -f 1_nginx-ingress/crds/
 k apply -f 1_nginx-ingress/manifest.yaml
 
+
+
 k -n nginx-ingress port-forward svc/nginx-ingress 80   #yaml
 
 change dns
@@ -13,6 +15,23 @@ k apply -f ingress-resource.yaml
 
 curl http://localhost
 
-curl --connect-to www.itau.com.br:80:127.0.0.1 http://www.itau.com.br
 
+
+curl --resolve www.demo.io:80:3.108.142.158 http://www.demo.io
+
+
+curl --connect-to www.itau.com.br:80:127.0.0.1 http://www.itau.com.br
 curl --connect-to www.itau.com.br::127.0.0.1 http://www.itau.com.br
+
+
+curl -v a2f324b078f4343cd8965e5c976cc24c-412dfb1aeafaca26.elb.us-east-1.amazonaws.com
+
+
+
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+
+helm install ingress-nginx ingress-nginx -n ingress-nginx
+
+
+helm install my-release oci://ghcr.io/nginxinc/charts/nginx-ingress --version 1.2.2 --set controller.image.repository=myregistry.example.com/nginx-plus-ingress --set controller.nginxplus=true
